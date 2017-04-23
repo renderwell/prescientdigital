2c59270ea137b9907e59f0bac1d0bba5a7b56275
renderwell <renderwell@gmail.com>
2017-04-23 12:34:47 +0100

    Modifications to banner viewlet, side navigation, styles etc.

--- src/pd.theme/pd/theme/browser/bannerimage.py
+++ src/pd.theme/pd/theme/browser/bannerimage.py
@@ -4,10 +4,20 @@ from plone.app.layout.viewlets.common import ViewletBase
 from plone.memoize import view
 from Products.CMFCore.utils import getToolByName
 from plone.memoize.instance import memoize
+from plone.app.layout.navigation.defaultpage import isDefaultPage
 
 
 class BannerImage(ViewletBase):
     render = ViewPageTemplateFile('bannerimage.pt')
 
-    def get_banner_image(self):
-        return 'banner image'
+    def banner_image_url(self):
+        return '%s/banner-image/image' % context.absolute_url()
+
+    def show(self):
+        default_page = isDefaultPage(self.context.aq_parent, self.context)
+        path_length = len(self.context.getPhysicalPath())
+
+        if default_page and path_length == 4:
+            return True
+        elif path_length == 3:
+            return True
