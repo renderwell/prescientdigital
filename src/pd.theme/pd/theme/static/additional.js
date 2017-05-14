d39943b4edd8c943cf59863ff33d8e1928fdf1d5
renderwell <renderwell@gmail.com>
2017-05-15 00:01:35 +0100

    load menu js async

--- src/pd.theme/pd/theme/static/additional.js
+++ src/pd.theme/pd/theme/static/additional.js
@@ -1,10 +1,1 @@
-
-$(document).on('ready', function() {
-    $('.menu.vertical > li').hover(function() {
-        var el = $(this);
-        var dropdown_height = el.find('.submenu').height();
-        if (dropdown_height > $(window).height()) {
-            el.addClass('max');
-        }
-    });
-});
+var x = 1;
