161c5fb7ef4b09cdecc2f83142d339f2b1602a95
ablumer <arnoblumer@gmail.com>
2017-05-15 18:20:35 +0100

    Fix menu template.

--- src/pd.theme/pd/theme/static/additional.js
+++ src/pd.theme/pd/theme/static/additional.js
@@ -1,1 +1,14 @@
-var x = 1;
+
+$(document).on('ready', function() {
+    var topnav = $('#topnav .pd-portal-globalnav');
+    var offnav = $('#offCanvas .pd-portal-globalnav');
+
+    // if (document.documentElement.clientWidth >= 1024) {
+    //     offnav.remove();
+    // }
+
+    var switchNav = function switchNavF(nav) {
+
+    });
+
+});
